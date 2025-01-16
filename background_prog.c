60bc0d9e09513244f589a9212e056503be2c92e0
Xelaphir <alex.lugovoy88@gmail.com>
2025-01-17 08:32:40 +1000

    Add template for library

---
+++ background_prog.c
@@ -0,0 +1,5 @@
+#include "background_prog.h"
+
+int addition(int a, int b){
+    return a + b;
+}
---
+++ test_background_prog.c
@@ -0,0 +1,8 @@
+#include "background_prog.h"
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    int ans = addition(7, 8);
+    printf("%d", ans);
+    return 0;
+}
