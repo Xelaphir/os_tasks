b40b2d3706bd2050267e09b39723eb776de64145
Xelaphir <alex.lugovoy88@gmail.com>
2025-01-17 08:35:29 +1000

    Test library

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
+#include "background_prog.h"
+
 #include <stdio.h>
 
 int main(int argc, char** argv){
     printf("Hello, from OsTasks!\n");
+    int ans = addition(7, 8);
+    printf("%d", ans);
+    return 0;
 }
